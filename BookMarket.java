8cfedc87ad3f154aeb471d6e073fed6361c580ca
hvxxn <162563757+hvxxn@users.noreply.github.com>
2024-03-12 16:01:40 +0900

    2

---
+++ BookMarket.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        // TODO Auto-generated method stub
+        System.out.println("Welcome to Shopping Mall!");
+        System.out.println("Welcome to BookMarket!!!");
+    }
+
+}
