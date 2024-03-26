9d798ce9aee10589cc373141cc2d25ba61fa7459
hvxxn <162563757+hvxxn@users.noreply.github.com>
2024-03-26 15:57:16 +0900

    1

--- Welcome.java
+++ Welcome.java
@@ -67,4 +67,11 @@ public class Welcome {
         } // while 끝
     } // main 함수 끝
 
+    public static void menuIntro() {
+        System.out.println("1. 고객정보 확인하기\t5. 바구니에 항목 추가하기");
+        System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니에 항목 수량 줄이기");
+        System.out.println("3. 장바구니 비우기\t7. 장바구니의 항목 삭제하기");
+        System.out.println("4. 영수증 표시하기\t8. 종료");
+        System.out.println("*********************************");
+    }
 } // Welcome 클래스 끝
